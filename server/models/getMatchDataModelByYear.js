fd17b446277d33821d45e7d4893aece88ffb25b2
Jacobk12345 <152231698+Jacobk12345@users.noreply.github.com>
2026-01-23 17:50:31 -0500

    Scouting Database is now starting up again
    - The 'node_modules' files in 'server' and 'client' were missing
    - The equivalent files from "ScoutingDatabase2025" were copied to "ScoutingDatabase2026"
    - The 'appdb.sqlite' was updated for 2026 game year
    - 'getMatchDataModelByYear.js' was updated to include 'MatchData2026'

--- server/models/getMatchDataModelByYear.js
+++ server/models/getMatchDataModelByYear.js
@@ -1,5 +1,6 @@
 const MatchData = require('./matchData2024');
 const MatchData2025 = require('./matchData2025');
+const MatchData2026 = require('./matchData2026');
 
 const getMatchDataModelByYear = (year) => {
   switch (year) {
@@ -7,6 +8,8 @@ const getMatchDataModelByYear = (year) => {
       return MatchData;
     case '2025':
       return MatchData2025;
+    case '2026':
+      return MatchData2026;
     default:
       throw new Error('Invalid year');
   }
