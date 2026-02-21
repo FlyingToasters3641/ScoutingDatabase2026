f3cfcb7eaeef06a799d7f82f26074b31799eb4fd
JackSlisher1234 <152231711+JackSlisher1234@users.noreply.github.com>
2026-02-20 20:05:48 -0500

    s

--- client/src/views/Settings/Settings.js
+++ client/src/views/Settings/Settings.js
@@ -52,7 +52,7 @@ import { Container, Row, Col, Modal } from "react-bootstrap";
                      <h3>App Information</h3>
                      <hr></hr>
                      <p>Flying Toasters ScoutDB Version: 2026.0.0 dev</p>
-                     <p>Most Recent Server Model Version: 2025-v0 dev</p>
+                     <p>Most Recent Server Model Version: 2026-v1 dev</p>
                      <p>Powered By <a href="https://www.thebluealliance.com/" target="_blank" rel="noopener noreferrer">The Blue Alliance</a></p>
                  </Col>
                  <Col md={4}>
